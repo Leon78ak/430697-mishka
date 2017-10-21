f44663c010c2cb6e2cefd655697caea6c8a0e68c
Alexandr Kokorev <ak_78leon@mail.ru>
2017-10-22 03:17:18 +0600

    script

---
+++ js/script--catalog.js
@@ -0,0 +1,24 @@
+var product = document.querySelector(".product");
+
+var toCart = product.querySelectorAll(".to-cart");
+
+var popupToCart = document.querySelector(".modal--to-cart");
+
+var overlay = document.querySelector(".overlay");
+
+for (var i = 0; i <= toCart.length; i++) {
+  toCart[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    popupToCart.classList.add("modal--show");
+    overlay.classList.add("overlay--show");
+  });
+};
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+    if ((popupToCart.classList.contains("modal--show")) & (overlay.classList.contains("overlay--show"))) {
+      popupToCart.classList.remove("modal--show");
+      overlay.classList.remove("overlay--show");
+    }
+  }
+});
